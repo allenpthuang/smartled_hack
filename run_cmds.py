19e39499c1cdf31b5408a7fa1b76563b39fc81b0
Pin-Tsung (Allen) Huang <allen.pt.huang@gmail.com>
2023-03-08 13:51:24 -0500

    Explore mechanisms for loops

--- run_cmds.py
+++ run_cmds.py
@@ -2,21 +2,31 @@ import asyncio
 import time
 from bleak import BleakClient
 
+def log_info(text):
+    print('[INFO] ' + text)
 
 async def run_cmds(address, commands):
-    print('=== START ===')
+    log_info('START')
     async with BleakClient(address) as client:
-        print('=== connected ===')
+        log_info('BT Connected')
+
         cmd_ptr = 0
         while cmd_ptr < len(commands):
-            print(f'Running cmd #{cmd_ptr + 1}')
+            log_info(f'Running cmd #{cmd_ptr + 1}')
             cmd = commands[cmd_ptr]
-            if 'goto' in cmd:
+            if 'goto' not in cmd:
+                cmd_ptr += 1
+            elif cmd['infinite'] or cmd['count'] > 0:
                 cmd_ptr = cmd['goto']
-                print(f'Going to cmd #{cmd_ptr + 1}...')
+                log_info(f'Going to cmd #{cmd_ptr + 1}...')
+                if not cmd['infinite']:
+                    cmd['count'] -= 1
                 continue
             else:
                 cmd_ptr += 1
+                cmd['count'] = cmd['orig_count']
+                log_info(f'Loop done! Count reset to {cmd["orig_count"]}.')
+                continue
 
             model_number = await client.write_gatt_char(uuid, cmd['gatt_cmd'])
             if cmd['duration'] <= 0:
@@ -25,7 +35,7 @@ async def run_cmds(address, commands):
             else:
                 time.sleep(cmd['duration'])
 
-    print('=== END ===')
+    log_info('END')
 
 # destination address
 address = '21:0a:00:06:5a:b2'
@@ -45,21 +55,32 @@ cmd_2 = {
         'power': 'ff', # on
         'color': '00ff00', # green
         'twinkle': '0004', # off
-        'duration': 7.5 # sleep after this command (in seconds)
+        'duration': 1 # sleep after this command (in seconds)
         }
 
 cmd_3 = {
         'power': 'ff', # on
         'color': '0000ff', # blue
         'twinkle': '0001', # off
-        'duration': 5.5 # sleep after this command (in seconds)
+        'duration': 1 # sleep after this command (in seconds)
         }
 
 cmd_4 = {
-    'goto': 1
+    'goto': 1,
+    'infinite': False,
+    'count': 3,
+    'orig_count': 3
 }
 
-cmds = [cmd_1, cmd_2, cmd_3, cmd_4]
+cmd_5 = {
+    'goto': 0,
+    'infinite': True,
+    'count': -1,
+    'orig_count': -1
+}
+
+# this runs like 1->2->3->4->(2->3)->4->(2->3)->4->(2->3)->4->5->1->....
+cmds = [cmd_1, cmd_2, cmd_3, cmd_4, cmd_5]
 
 commands = []
 for cmd in cmds:
